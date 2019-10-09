2a30dbb095fdbfc82ba43cf381ec34e8a2343f50
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-09 16:01:52 +0200

    [Python][Test] use unittest

--- python/test/quick.py
+++ python/test/quick.py
@@ -1,13 +1,22 @@
-from libparametric_curves_pywrap import polynomial, curve_constraints, forcecurve, spline
+import unittest
 
 from numpy import matrix
 
-cc = curve_constraints()
-cc.init_vel = matrix(range(3)).T
+from libparametric_curves_pywrap import (curve_constraints, forcecurve, polynomial, spline)
 
-fc = forcecurve()
-sp = spline()
-pn = polynomial(matrix('1 2 3;4 5 6;7 8 9'))
 
-assert pn.min() == 0.0
-assert pn.max() == 1.0
+class ParametricCurvesQuickTests(unittest.TestCase):
+    def test_quick(self):
+        cc = curve_constraints()
+        cc.init_vel = matrix(range(3)).T
+
+        forcecurve()
+        spline()
+        pn = polynomial(matrix('1 2 3;4 5 6;7 8 9'))
+
+        self.assertEqual(pn.min(), 0.0)
+        self.assertEqual(pn.max(), 1.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
