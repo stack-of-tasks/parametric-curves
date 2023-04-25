ce3ba8f0ffbda2353ccb66385b0cfe94438a1b82
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-04-25 12:20:51 +0200

    switch to numpy arrays

--- tests/python/quick.py
+++ tests/python/quick.py
@@ -1,7 +1,6 @@
 import unittest
 
-import eigenpy
-from numpy import matrix
+from numpy import array
 
 from libparametric_curves_pywrap import (
     curve_constraints,
@@ -10,17 +9,15 @@ from libparametric_curves_pywrap import (
     spline,
 )
 
-eigenpy.switchToNumpyMatrix()
-
 
 class ParametricCurvesQuickTests(unittest.TestCase):
     def test_quick(self):
         cc = curve_constraints()
-        cc.init_vel = matrix(range(3)).T
+        cc.init_vel = array(range(3))
 
         forcecurve()
         spline()
-        pn = polynomial(matrix("1 2 3;4 5 6;7 8 9"))
+        pn = polynomial(array("1 2 3;4 5 6;7 8 9"))
 
         self.assertEqual(pn.min(), 0.0)
         self.assertEqual(pn.max(), 1.0)
